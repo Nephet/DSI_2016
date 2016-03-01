c0d45da5e5fd96ed21006b6360a5284ae866227f
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-03-01 15:44:53 +0100

    throw and multi input

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -7,4 +7,45 @@ public class PlayerActions : MonoBehaviour {
 	public GameObject currentBall;
 
 	public List<GameObject> balls;
+
+    GameObject mesh;
+
+    public float throwPower = 5f;
+
+    public static int nbPlayers;
+    public int id;
+
+    void Awake()
+    {
+        nbPlayers++;
+
+        id = nbPlayers;
+    }
+
+    void Start()
+    {
+        mesh = GetComponent<Movement>().mesh;
+
+        if (currentBall)
+        {
+            currentBall.GetComponent<Rigidbody>().isKinematic = true;
+            currentBall.transform.parent = mesh.transform;
+            currentBall.transform.position = transform.position + mesh.transform.forward/2;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("A_Button_"+id) && currentBall)
+        {
+            Throw(throwPower);
+        }
+    }
+
+    void Throw(float power)
+    {
+        currentBall.GetComponent<Rigidbody>().isKinematic = false;
+        currentBall.transform.parent = null;
+        currentBall.GetComponent<Rigidbody>().AddForce(mesh.transform.forward * power, ForceMode.Impulse);
+    }
 }
