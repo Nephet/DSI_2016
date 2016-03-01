072e86f047225f5ae9e4590dd2e65cebc11d413f
benoit vidot <b.vidot@rubika-edu.com>
2016-03-01 16:09:25 +0100

    add snap

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -41,7 +41,6 @@ public class Movement : MonoBehaviour {
 
 		Debug.DrawRay (transform.position, _directionAlt, Color.red);
 		_rotation = Quaternion.LookRotation (_directionAlt, transform.up);
-		Debug.Log (_directionAlt);
 
 	}
 
--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -6,5 +6,51 @@ public class PlayerActions : MonoBehaviour {
 
 	public GameObject currentBall;
 
-	public List<GameObject> balls;
+	float nearestdistance = Mathf.Infinity;
+	GameObject nearestBall;
+	GameObject mesh;
+
+	bool Snap;
+	public float rangeSnap = 1.0f;
+
+
+	void Start()
+	{
+		mesh = transform.GetComponent<Movement> ().mesh;
+	}
+
+	void Update()
+	{
+		Snap = Input.GetButtonDown ("A_Button_1");
+		Debug.Log (Snap);
+		if (Snap) {
+			DistanceBalls ();
+			if (nearestBall != null) {
+				currentBall = nearestBall;
+				currentBall.GetComponent<Rigidbody> ().isKinematic = true;
+				currentBall.transform.parent = mesh.transform;
+			}
+
+		}
+			
+		if(currentBall!=null){
+			
+			Debug.Log (currentBall);
+			currentBall.transform.position = transform.position + currentBall.transform.forward /2;
+
+		}
+
+
+	}
+
+	void DistanceBalls()
+	{
+		for (int i = 0; i < BallsManager.instance.balls.Count; i++) 
+		{
+			float distance = Vector3.Distance (transform.position, BallsManager.instance.balls [i].transform.position);
+			if (distance < nearestdistance && distance <= rangeSnap) {
+				nearestBall = BallsManager.instance.balls [i].gameObject;
+			}
+		}
+	}
 }
