c539f2626d6e514188a51f1d99e75988b2848ff4
benoit vidot <b.vidot@rubika-edu.com>
2016-03-01 09:30:44 +0100

    Add deplacement joueur

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour {
+
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float _horizontal = Input.GetAxisRaw ("Horizontal");
+		float _vertical = Input.GetAxisRaw ("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _horizontal;
+		Vector3 _movVertical = transform.right * _vertical;
+
+		float _velocity = (_movHorizontal * _movVertical).normalized * speed;
+	}
+
+	void FixedUpdate()
+	{
+		PerformMovement ();
+
+	}
+
+	void PerformMovement()
+	{
+
+	}
+}
