aef1f6d34270572d0e42f9f406e0e579409cb3d9
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-03-03 14:49:48 +0100

    pinatas added

---
+++ Assets/Scripts/Pinata.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pinata : MonoBehaviour {
+
+    public int teamId;
+
+	// Use this for initialization
+	void Start () {
+        teamId = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        PlayerActions pA = other.transform.GetComponent<PlayerActions>();
+
+        if (!pA || pA.state == PlayerActions.State.HUMAN) return;
+
+        ChangeTeam(other.transform.GetComponent<Ball>().idTeam);
+        
+    }
+
+    void ChangeTeam(int id)
+    {
+        teamId = id;
+
+        PinataManager.instance.CheckEffect();
+        
+        ChangeColor(id == 1 ? Color.red : id == 2 ? Color.blue : Color.white);
+    }
+
+    void ChangeColor(Color c)
+    {
+        Debug.Log(GetComponentInChildren<MeshRenderer>().material.color + " " + c);
+        GetComponentInChildren<MeshRenderer>().material.color = c;
+    }
+}
---
+++ Assets/Scripts/PinataManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinataManager : MonoBehaviour {
+
+    #region Singleton
+    static private PinataManager s_Instance;
+    static public PinataManager instance
+    {
+        get
+        {
+            return s_Instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (s_Instance == null)
+            s_Instance = this;
+        //DontDestroyOnLoad (this);
+    }
+
+    #endregion
+
+    public Pinata[] pinatas;
+
+	// Use this for initialization
+	void Start () {
+        pinatas = GameObject.FindObjectsOfType<Pinata>();
+	}
+
+    public int CheckPinatas()
+    {
+        int nbTeam1 = 0;
+        int nbTeam2 = 0;
+
+        int res = 0;
+
+        foreach(Pinata p in pinatas)
+        {
+            if(p.teamId == 1)
+            {
+                nbTeam1++;
+            }
+            else if(p.teamId == 2)
+            {
+                nbTeam2++;
+            }
+        }
+
+        if (nbTeam1 >= 3)
+        {
+            res = 1;
+        }
+        else if (nbTeam2 >= 3)
+        {
+            res = 2;
+        }
+        else
+        {
+            res = 0;
+        }
+
+        return res;
+
+    }
+
+    public void CheckEffect()
+    {
+        switch (CheckPinatas())
+        {
+            case 0:
+                Debug.Log("nobody has it");
+                break;
+            case 1:
+                Debug.Log("team 1 has it");
+                break;
+            case 2:
+                Debug.Log("team 2 has it");
+                break;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
