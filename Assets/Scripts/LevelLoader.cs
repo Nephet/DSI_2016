97418429a7e1eba80743aa1272916ca17bcd17c9
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-03-04 15:29:17 +0100

    load level added

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+    
+	// Use this for initialization
+	void Awake () {
+	    if(Application.loadedLevel != 0)
+        {
+            Application.LoadLevel(0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/RotateTerrain.cs
+++ Assets/Scripts/RotateTerrain.cs
@@ -5,7 +5,7 @@ public class RotateTerrain : MonoBehaviour {
 
 	public float speed;
 
-    int _direction = 1;
+    public int direction = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +14,7 @@ public class RotateTerrain : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		_direction = MatchManager.Instance.direction;
-		transform.Rotate(new Vector3(0f,speed * Time.deltaTime * _direction, 0f));
+		//_direction = MatchManager.Instance.direction;
+		transform.Rotate(new Vector3(0f,speed * Time.deltaTime * direction, 0f));
 	}
 }
