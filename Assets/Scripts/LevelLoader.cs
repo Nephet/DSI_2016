612856193f554a11caa136f69ad4eb6d46144177
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-03-04 15:36:00 +0100

    bug correction

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -2,12 +2,28 @@
 using System.Collections;
 
 public class LevelLoader : MonoBehaviour {
-    
-	// Use this for initialization
-	void Awake () {
-	    if(Application.loadedLevel != 0)
+
+    static private LevelLoader s_Instance;
+    static public LevelLoader instance
+    {
+        get
+        {
+            return s_Instance;
+        }
+    }
+
+    static bool b = false;
+
+    // Use this for initialization
+    void Awake () {
+        if (s_Instance == null)
+            s_Instance = this;
+        DontDestroyOnLoad(this);
+
+        if (Application.loadedLevel != 0 && !b)
         {
             Application.LoadLevel(0);
+            b = true;
         }
 	}
 	
