9a090088d88bd8745d6122a8536177e36dde8bb0
VILLECROZE Antoine <a.villecroze@rubika-edu.com>
2016-03-09 15:47:56 +0100

    Ajout des Footsteps

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour {
+
+    public GameObject leftFootprint;
+    public GameObject rightFootprint;
+
+    public float offset = 0.05f;
+
+    public Transform leftFootLocation;
+    public Transform rightFootLocation;
+
+  /*  public AudioSource leftFootAudio;
+    public AudioSource rightFootAudio;*/
+    
+
+    void LeftFootstep()
+    {
+        //leftFootAudio.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(leftFootLocation.position, leftFootLocation.forward, out hit))
+        {
+            Instantiate(leftFootprint, hit.point + hit.normal*offset, Quaternion.LookRotation(hit.normal, leftFootLocation.up));
+        }
+    }
+
+    void RightFootstep()
+    {
+        //rightFootAudio.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rightFootLocation.position, rightFootLocation.forward, out hit))
+        {
+            Instantiate(rightFootprint, hit.point + hit.normal * offset, Quaternion.LookRotation(hit.normal, rightFootLocation.up));
+        }
+    }
+
+}
